0a41e88bf45833c7159015fa2a5318fd1d7321f5
Adarsh Kumar Hembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-04-16 12:07:41 +0000

    Es6

---
+++ 1.ES6/2.js
@@ -0,0 +1,12 @@
+// Create an object person with two properties, "name" and "age" and then updates the "age"
+// property to a new value. Initial age should be 30
+
+let person={
+    name: "Adarsh",
+    age: 35
+};
+
+console.log(person.age);
+
+// 2. Write a function that takes an object car and returns true if the car is a sports car (i.e. has a
+// horsepower property greater than or equal to 300)
