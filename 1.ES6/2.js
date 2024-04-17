27025e66ca5e615fc5d3b3e89e4d2c54f268e9bc
Adarsh Kumar Hembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-04-17 02:40:05 +0000

    Practice some question

--- 1.ES6/2.js
+++ 1.ES6/2.js
@@ -10,3 +10,16 @@ console.log(person.age);
 
 // 2. Write a function that takes an object car and returns true if the car is a sports car (i.e. has a
 // horsepower property greater than or equal to 300)
+function isSportsCar(car) {
+    if (car && typeof car === 'object' && 'horsepower' in car) {
+        return car.horsepower >= 300;
+    } else {
+        return false; // Return false if car is not a valid object or if it doesn't have the 'horsepower' property
+    }
+}
+
+const car1 = { make: 'Porsche', model: '911', horsepower: 450 };
+const car2 = { make: 'Toyota', model: 'Camry', horsepower: 200 };
+
+console.log(isSportsCar(car1)); // Output: true
+console.log(isSportsCar(car2)); // Output: false
