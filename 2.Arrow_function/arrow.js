73b815b5bc04b75f8782415f77cdb8c2188ed172
Adarsh Kumar Hembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-04-18 10:20:43 +0000

    arrow function

---
+++ 2.Arrow_function/arrow.js
@@ -0,0 +1,11 @@
+// const sayhello = function(name){
+//     return "hi there", + name + "!"
+// }
+
+// console.log(sayhello('sammy'));
+
+const sayhello = (name)=>{
+    return "hi there", + name + "!"
+}
+
+console.log(sayhello('sammy'));
