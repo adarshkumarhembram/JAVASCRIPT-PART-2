8f75858f21ad5c0c5d8765fcf5c6a4f30ac2cdda
Adarsh Kumar Hembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-04-23 03:35:11 +0000

    comparision

--- js/index.js
+++ js/index.js
@@ -63,3 +63,31 @@ for (var i = 0; i < 3; i++) {
 }
 
 // output - 3,3,3
+
+
+
+// Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//     - 4 > 3 true
+//     - 4 >= 3 true
+//     - 4 < 3 false
+//     - 4 <= 3 false
+//     - 4 == 4 true
+//     - 4 === 4 true
+//     - 4 != 4 false
+//     - 4 !== 4 false
+//     - 4 != '4' false
+//     - 4 == '4' true
+//     - 4 === '4' false
+//     - Find the length of python and jargon and make a falsy comparison statement.
+
+console.log(4 > 3);
+console.log(4 >= 3);
+console.log(4 < 3);
+console.log(4 <= 3);
+console.log(4 == 4);
+console.log(4 === 4);
+console.log(4 != 4);
+console.log(4 !== 4);
+console.log(4 != "4");
+console.log(4 == "4");
+console.log(4 === "4");
