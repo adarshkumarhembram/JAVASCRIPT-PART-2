0af952ceb5dacb040ce3bbdbf18bc6ec4a76b88b
Adarsh Kumar Hembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-04-24 10:57:09 +0000

    now

--- js/index.js
+++ js/index.js
@@ -91,3 +91,11 @@ console.log(4 !== 4);
 console.log(4 != "4");
 console.log(4 == "4");
 console.log(4 === "4");
+
+
+// 04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+let str = "Hello my name is Jagdish";
+
+console.log(str.includes("Jagdish"));
+console.log(str.includes("jagdish"));
