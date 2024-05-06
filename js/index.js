853d335561e06fdbfe941f6d25ec366fed043877
Adarsh Kumar Hembram <151887139+adarshkumarhembram@users.noreply.github.com>
2024-05-06 17:32:36 +0000

    JS

--- js/index.js
+++ js/index.js
@@ -216,3 +216,20 @@ for (let i = 0; i < shoppingCart.length; i++) {
 }
 
 console.log(shoppingCart);
+
+
+//  Write a program to create two array "even" and "odd" having even and odd number between the 0 to 100 (0 and 100 included).
+
+let odd = [];
+let even = [];
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 == 0) {
+    even.push(i);
+  } else {
+    odd.push(i);
+  }
+}
+
+console.log(odd);
+console.log(even);
